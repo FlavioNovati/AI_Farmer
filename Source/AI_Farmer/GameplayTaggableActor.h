1edec2ddfb2c94a1980495b6baf7dbd80f1d5adf
FlavioNovati <f.novati10@gmail.com>
2026-02-13 14:47:13 +0100

    Base Behaviour

---
+++ Source/AI_Farmer/GameplayTaggableActor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayTagAssetInterface.h"
+#include "GameplayTaggableActor.generated.h"
+
+UCLASS()
+class AI_FARMER_API AGameplayTaggableActor : public AActor, public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGameplayTaggableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FGameplayTagContainer GameplayTagContainer;
+
+	// Inherited via IGameplayTagAssetInterface
+	void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+};
