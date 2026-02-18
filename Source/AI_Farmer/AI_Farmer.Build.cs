a635e7abf89010ea0262a64a3268f06d8ca69fec
FlavioNovati <f.novati10@gmail.com>
2026-02-18 19:02:33 +0100

    Turret

--- Source/AI_Farmer/AI_Farmer.Build.cs
+++ Source/AI_Farmer/AI_Farmer.Build.cs
@@ -8,7 +8,7 @@ public class AI_Farmer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/AI_Farmer/GenericTeamAgentActor.cpp
@@ -0,0 +1,8 @@
+#include "GenericTeamAgentActor.h"
+
+AGenericTeamAgentActor::AGenericTeamAgentActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
