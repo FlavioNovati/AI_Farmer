b48e330aba540fdb42188f0af059e3a66394d847
FlavioNovati <f.novati10@gmail.com>
2026-02-18 19:27:05 +0100

    Removed Unused Class

--- Source/AI_Farmer/GenericTeamAgentActor.cpp
+++
@@ -1,8 +0,0 @@
-#include "GenericTeamAgentActor.h"
-
-AGenericTeamAgentActor::AGenericTeamAgentActor()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
